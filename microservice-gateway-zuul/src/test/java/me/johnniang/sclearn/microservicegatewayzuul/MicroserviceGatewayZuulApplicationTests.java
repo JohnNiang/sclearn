32d8769c4844f7d78de4a5c6c5083a25cb4ed69b
johnniang <johnniang@foxmail.com>
2020-06-08 16:38:41 +0800

    Provide zuul feature

---
+++ microservice-gateway-zuul/src/test/java/me/johnniang/sclearn/microservicegatewayzuul/MicroserviceGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package me.johnniang.sclearn.microservicegatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
