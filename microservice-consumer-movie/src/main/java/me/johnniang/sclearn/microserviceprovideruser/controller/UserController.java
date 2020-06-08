2520e33c774dfe511ef0f88e55b0ea70d946b340
johnniang <johnniang@foxmail.com>
2020-06-08 15:29:54 +0800

    Provide hystrix support

--- microservice-consumer-movie/src/main/java/me/johnniang/sclearn/microserviceprovideruser/controller/UserController.java
+++ microservice-consumer-movie/src/main/java/me/johnniang/sclearn/microserviceprovideruser/controller/UserController.java
@@ -1,11 +1,13 @@
 package me.johnniang.sclearn.microserviceprovideruser.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import me.johnniang.sclearn.microserviceprovideruser.entity.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -16,8 +18,13 @@ public class UserController {
         this.userFeignClient = userFeignClient;
     }
 
+    @HystrixCommand(fallbackMethod = "findByIdFallback")
     @GetMapping("users/{userId:\\d+}")
     public User findById(@PathVariable("userId") Long userId) {
         return userFeignClient.findById(userId);
+    }
+
+    public User findByIdFallback(Long userId) {
+        return new User(userId, "your_name", "Your name", 0, BigDecimal.ZERO);
     }
 }
